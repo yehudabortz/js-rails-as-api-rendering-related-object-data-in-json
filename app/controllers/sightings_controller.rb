ecf0286f42b4dbeeec56804d88191950df679439
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-22 10:19:46 -0500

    commit tree d932b233848e57657e205d9e374e82d09adfe916

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,18 @@
+class SightingsController < ApplicationController
+
+     def index
+        sightings = Sighting.all
+        render json: sightings, include: [:bird, :location]
+     end
+
+    def show
+        sighting = Sighting.find_by(id: params[:id])
+        # render json: sighting
+        # render json: {id: sighting.id, bird: sighting.bird, location: sighting.location }
+        if sighting
+            render json: sighting, include: [:bird, :location]
+        else
+            render json: {message: "No sighting"}
+        end
+    end
+end
